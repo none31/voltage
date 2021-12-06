30f7f4916da61d2cdd5d1689651e477944eaa96f
none31 <95595023+none31@users.noreply.github.com>
2021-12-06 19:17:14 +0700

    Update model.js

--- api/sdk/model.js
+++ api/sdk/model.js
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/trade_dnn/main/public/model_test/model.json';
+        const path = 'https://raw.githubusercontent.com/none31/voltage/main/public/tfjs_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
