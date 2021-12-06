d349c4d8b181e667a9430252a130ffe9df2894a2
none31 <95595023+none31@users.noreply.github.com>
2021-12-06 20:16:34 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,18 +16,42 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict`
     );   
 });
 
-bot.onText(/\/menu/, (msg) => { 
-    console.log(msg)
+state=0
+bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
+        `input the value i|v`
     );   
+    state=1;
 });
 
+bot.on('message', (msg) => {
+    if(state==1){
+        console.log(msg.text);
+        s=msg.text.split('|');
+        i=s[0]
+        v=s[1]
+        model.predict(
+            [parsefloat(s[0]), parsefloat(s[1])]
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,'predicted v is ${jres[0]} volt'
+            );
+            bot.sendMessage(
+                msg.chat.id,'predicted p is ${jres[1]} watt'
+            );
+            bot.sendMessage(
+                msg.chat.id,`/predict again`
+        );   
+        })
+    }else{
+        state=0
+    }
+})
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
     model.predict(
